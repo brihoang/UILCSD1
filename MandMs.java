a805b536325b17afe60a45d9b31c8e7b14a9a200
Brian <bri.hoang@gmail.com>
2014-04-05 17:02:11 -0500

    added MandMs

---
+++ MandMs.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.System.*;
+public class MandMs{
+	
+	public static final HashSet<Integer> PRIMES = new HashSet<Integer>(); //improve efficiency
+	public static final HashSet<Integer> COMPOSITES = new HashSet<Integer>(); 
+
+	public static void main(String[] args) throws IOException{
+		Scanner in = new Scanner(new File("mandms.dat"));	
+		int numTimes = in.nextInt();
+		in.nextLine();
+
+
+		for (int its = 1; its <= numTimes; its++){
+			int x = in.nextInt();
+			out.println(numMouthfuls(x));
+		}
+	}
+
+	public static int numMouthfuls(int n){
+		if( n <=3 )
+			return n;
+		else if( isPrime(n) )
+			return 1 + numMouthfuls(n-1);
+		else
+			return 1 + numMouthfuls(rowSize(n));
+	
+	}
+
+	public static int rowSize(int n){
+		for(int i = 2; i < n; i++){
+			if(n % i == 0)
+				return i;
+		}	
+		return -1;
+	}
+
+	public static boolean isPrime(int n){
+		if (n == 2 || PRIMES.contains(n))
+			return true;
+		if(n%2 == 0 || COMPOSITES.contains(n))
+			return false;
+
+		for(int i = 3; i < Math.sqrt(n); i+=2){
+			if(n % i == 0){
+				COMPOSITES.add(n);
+				return false;
+			}
+		}
+		PRIMES.add(n);
+		return true;
+	}
+}

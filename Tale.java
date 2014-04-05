25e9777e334211f9956678c8bc37d7beaaf19c2d
Brian <bri.hoang@gmail.com>
2014-04-05 04:42:05 -0500

    added tale

---
+++ Tale.java
@@ -0,0 +1,11 @@
+public class Tale{
+	public static void main(String[] args){
+		System.out.println("This is a");	
+		System.out.println(" very long");
+		System.out.println("  and un-");
+		System.out.println(" happy");
+		System.out.println("tale");
+		System.out.println("  .");
+	}
+
+}

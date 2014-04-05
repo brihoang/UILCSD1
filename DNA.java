6ba78923ef77b108621e3b40d63651c3a9806018
Brian <bri.hoang@gmail.com>
2014-04-05 01:51:21 -0500

    added DNA

---
+++ DNA.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.System.*;
+public class DNA{
+	public static void main(String[] args) throws IOException{
+		Scanner in = new Scanner(new File("dna.dat"));	
+		int numTimes = in.nextInt();
+		in.nextLine();
+
+
+		for (int its = 1; its <= numTimes; its++){
+			String strand1 = in.nextLine().toUpperCase().trim();
+			String strand2 = in.nextLine().toUpperCase().trim();
+
+			if(strand1.length() != strand2.length()){
+				out.println("BAD");
+				continue;
+			}
+
+			boolean good = true;
+			for(int i = 0; i < strand1.length(); i++){
+				char c1 = strand1.charAt(i);
+				char c2 = strand2.charAt(i);
+
+				if(!(c1 == 'A' && c2 == 'T' || c1 == 'T' && c2 == 'A' || c1 == 'C' && c2 == 'G' || c1 == 'G' && c2 == 'C')){
+					out.println("BAD");
+					good = false;
+					break;
+				}
+					
+			}
+			if(good)
+				out.println("GOOD");
+		}
+	}
+}
